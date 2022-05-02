8ca3460449fe48536b1b40b428c95c260a7e9192
amvasanthsurya <amvasanthsurya@gmail.com>
2022-05-02 13:11:39 +0530

    Few lines of code added

---
+++ src/org/sample/Vasanth.java
@@ -0,0 +1,20 @@
+package org.sample;
+
+import java.util.Scanner;
+
+public class Vasanth {
+	
+	public static void main(String[] args) {
+		
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter your name");
+		String name=s.next();
+		System.out.println("enter your age");
+		int age=s.nextInt();
+		
+		
+		System.out.println("name" + name);
+		System.out.println("age" + age);
+	}
+
+}
