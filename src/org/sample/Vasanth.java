ba068996218dc4af3405f04076494080d8d5c64a
amvasanthsurya <amvasanthsurya@gmail.com>
2022-05-05 14:47:36 +0530

    added

--- src/org/sample/Vasanth.java
+++ src/org/sample/Vasanth.java
@@ -4,17 +4,18 @@ import java.util.Scanner;
 
 public class Vasanth {
 	
-	public static void main(String[] args) {
-		
-		Scanner s=new Scanner(System.in);
-		System.out.println("Enter your name");
-		String name=s.next();
-		System.out.println("enter your age");
-		int age=s.nextInt();
-		
-		
-		System.out.println("name" + name);
-		System.out.println("age" + age);
+	private void empId() {
+
+		System.out.println("empId: 8210");
 	}
 
+	
+	public static void main(String[] args) {
+		
+		Vasanth v=new Vasanth();
+		
+		v.empId();		
+		
+
+	}
 }
